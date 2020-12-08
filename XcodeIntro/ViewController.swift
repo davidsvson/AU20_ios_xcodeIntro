e6f5be7b32da1317f032aa38f7cbbd3f060d1763
David Svensson <davidsvson@gmail.com>
2020-12-08 14:14:29 +0100

    first commit

--- XcodeIntro/ViewController.swift
+++ XcodeIntro/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    var counter = 0
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        welcomeLabel.text = String(counter)
+        
+        print("Hej nu testar jag min första app")
     }
-
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        //sender.setTitle("tryckt", for: .normal)
+        button.setTitle("tryckt", for: .normal)
+        counter += 1
+        welcomeLabel.text = String(counter)
+        print("knappen trycks")
+    }
+    
 
 }
 
